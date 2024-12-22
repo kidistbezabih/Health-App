87988570a967a64aa9054dd8f73b3d13e8296787
Kidistbezabih <kidistbezabh@gmail.com>
2024-12-22 21:59:59 +0300

    <feat> add preexamination validator on the route

--- src/routers/preExaminationRoures.ts
+++ src/routers/preExaminationRoures.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { PreExaminationController } from "../controllers/preExaminationController";
+import { PreExaminationValidator } from "../validators/preExaminationValidator";
 
 export class PreExaminationRoute {
   static get routes(): Router {
     const router = Router();
     const preExaminationController = new PreExaminationController();
 
-    router.get('/next-patient', preExaminationController.ProceedToNextPateient);
-    router.post('/record-patient-symptomes', preExaminationController.recordPatientSymptoms);
-    
+    router.get('/next-patient' ,preExaminationController.ProceedToNextPateient);
+    router.post('/record-patient-symptomes', PreExaminationValidator.onCreate(), preExaminationController.recordPatientSymptoms);
     return router;
   }
 }
+
